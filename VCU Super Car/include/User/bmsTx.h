6a25e680e46f6ca169bb4a80c8fee26d234d5da8
SharyfIsmail <xandrey95x@live.ru>
2020-03-02 18:30:45 +0300

    handles data from bms

---
+++ VCU Super Car/include/User/bmsTx.h
@@ -0,0 +1,16 @@
+/*
+ * bmsTx.h
+ *
+ *  Created on: 25 февр. 2020 г.
+ *      Author: User
+ */
+
+#ifndef INCLUDE_USER_BMSTX_H_
+#define INCLUDE_USER_BMSTX_H_
+
+
+void BmsTxInit(void);
+
+
+
+#endif /* INCLUDE_USER_BMSTX_H_ */
---
+++ VCU Super Car/source/User/bmsTx.c
@@ -0,0 +1,41 @@
+/*
+ * BatteryManagementSystemTx.c
+ *
+ *  Created on: 25 февр. 2020 г.
+ *      Author: User
+ */
+
+
+#include "FreeRTOS.h"
+#include "os_queue.h"
+#include "os_task.h"
+#include "newCanLib.h"
+
+void vBmsTxHandler (void *pvParameters);
+
+
+
+TaskHandle_t xBmsTxHandler;
+QueueHandle_t xQueueBmsTx = NULL;
+
+void BmsTxInit(void)
+{
+    if(xTaskCreate(vBmsTxHandler, "BmsTxHandler", configMINIMAL_STACK_SIZE, NULL, 1, &xBmsTxHandler ) != pdTRUE)
+    {
+        /*Task couldn't be created */
+        while(1);
+    }
+    xQueueBmsTx =  xQueueCreate(20U, sizeof(canMessage_t));
+}
+
+void vBmsTxHandler (void *pvParameters)
+{
+    while(1)
+    {
+        taskYIELD();
+    }
+}
+
+
+
+
