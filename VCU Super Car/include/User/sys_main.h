090dbf191b17243db36093d5779aae245c11b03b
SharyfIsmail <xandrey95x@live.ru>
2020-03-02 18:29:28 +0300

    new project

---
+++ VCU Super Car/include/User/sys_main.h
@@ -0,0 +1,16 @@
+/*
+ * sys_main.h
+ *
+ *  Created on: 25 февр. 2020 г.
+ *      Author: User
+ */
+
+#ifndef INCLUDE_USER_SYS_MAIN_H_
+#define INCLUDE_USER_SYS_MAIN_H_
+
+
+
+
+
+
+#endif /* INCLUDE_USER_SYS_MAIN_H_ */
