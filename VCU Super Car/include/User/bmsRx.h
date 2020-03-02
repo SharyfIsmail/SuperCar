ac7225e99344b00738d5b2e7df9bb3cd0d3a3a29
SharyfIsmail <xandrey95x@live.ru>
2020-03-02 18:30:31 +0300

    sends data to bms

---
+++ VCU Super Car/include/User/bmsRx.h
@@ -0,0 +1,17 @@
+/*
+ * bmsRx.h
+ *
+ *  Created on: 25 февр. 2020 г.
+ *      Author: User
+ */
+
+#ifndef INCLUDE_USER_BMSRX_H_
+#define INCLUDE_USER_BMSRX_H_
+
+#define
+
+void BmsRxInit(void);
+
+
+
+#endif /* INCLUDE_USER_BMSRX_H_ */
