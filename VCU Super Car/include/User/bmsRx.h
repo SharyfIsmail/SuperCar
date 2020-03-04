768f22e8bbcee1911158dcf61c46f74a48810354
SharyfIsmail <xandrey95x@live.ru>
2020-03-04 17:03:16 +0300

    heartbeat was set and contactor requset,
    some inline function were added to manupilate with canmessageTx

--- VCU Super Car/include/User/bmsRx.h
+++ VCU Super Car/include/User/bmsRx.h
@@ -8,8 +8,67 @@
 #ifndef INCLUDE_USER_BMSRX_H_
 #define INCLUDE_USER_BMSRX_H_
 
+#include <stdint.h>
+#include "newCanLib.h"
+
 void BmsRxInit(void);
 
+#define CAN_PERIOD_MS_BMS_HEARTBEAT           ((uint32_t) 200)
+#define BMS_HEARTBEAT                         ((uint32_t) 0x18FF0213)
+#define BMS_HEARTBEAT_DLC                     ((uint8_t) 2)
+
+#define CAN_PERIOD_MS_BMS_CONTACTOR_REQUEST   ((uint32_t) 200)
+#define BMS_CONTACTOR_REQUEST                 ((uint32_t) 0x18FF0203)
+#define BMS_CONTACTOR_REQUEST_DLC             ((uint8_t) 3)
+
+
+/**
+ * @typedef BmsContactorRequest_t
+ * @brief Switching the battery
+ * */
+typedef enum
+{
+    CONTACTOR_OFF = 0x00, /*!< Specifies off mode. */
+    CONTACTOR_ON = 0x01  /*!< Specifies on mode. */
+}BmsContactorRequest_t;
+
+/**
+ * @typedef PcuFault_t
+ * @brief fixing the fault
+ * */
+typedef enum
+{
+    NO_PCU_FAULT = 0x0,
+    PCU_FAULT = 0x1
+}PcuFault_t;
+
+/**
+ * @typedef CriticalPcuFault_t
+ * @brief fixing the critical fault
+ * */
+typedef enum
+{
+    NO_CRITICAL_PCU_FAULT = 0x0,
+    CRITICAL_PCU_FAULT = 0x1
+}CriticalPcuFault_t;
+
+/*Contactor request length : 1 start bit : 8*/
+inline void setBmsContactorRequest(canMessage_t *ptr, uint8_t value)
+{
+    ptr->data[1] = (ptr->data[1] & 0xFE ) |  ((uint8_t) value & 0x01);
+}
+
+/*Pcu Fault length : 1 start bit : 16*/
+inline void setPcuFault(canMessage_t *ptr, uint8_t value)
+{
+    ptr->data[2] = (ptr->data[2] & 0xFE ) |  ((uint8_t) value & 0x01);
+}
+
+/*Pcu Critical Fault length : 1 start bit : 20*/
+inline void setPcuCriticalFault(canMessage_t *ptr, uint8_t value)
+{
+    ptr->data[2] = (ptr->data[2] & 0xEF ) |  ((uint8_t) value & 0x10);
+}
 
 
 #endif /* INCLUDE_USER_BMSRX_H_ */
--- VCU Super Car/source/User/bmsRx.c
+++ VCU Super Car/source/User/bmsRx.c
@@ -8,55 +8,75 @@
 #include "os_queue.h"
 #include "os_task.h"
 #include "newCanLib.h"
+#include "bmsRx.h"
+#include "string.h"
 
 void vBmsRxHandler (void *pvParameters);
 void vBmsHeartBeat (void *pvParameters);
 
 
 TaskHandle_t xBmsRxHandler;
-TaskHandle_t xvBmsHeartBeat;
-
 void BmsRxInit(void)
 {
-    if(xTaskCreate(vBmsRxHandler, "BmsRxHandler", configMINIMAL_STACK_SIZE, NULL, 1, &xBmsRxHandler ) != pdTRUE)
+    if(xTaskCreate(vBmsRxHandler, "BmsRxHandler", configMINIMAL_STACK_SIZE, (void *)CAN_PERIOD_MS_BMS_CONTACTOR_REQUEST, 1, &xBmsRxHandler ) != pdTRUE)
     {
         /*Task couldn't be created */
         while(1);
     }
-    if(xTaskCreate(vBmsHeartBeat, "BmsHeartBeat", configMINIMAL_STACK_SIZE, NULL, 1, &xvBmsHeartBeat ) != pdTRUE)
+    if(xTaskCreate(vBmsHeartBeat, "BmsHeartBeat", configMINIMAL_STACK_SIZE, (void *)CAN_PERIOD_MS_BMS_HEARTBEAT, 1, NULL) != pdTRUE)
     {
         /*Task couldn't be created */
         while(1);
     }
-
-
 }
 
 void vBmsRxHandler (void *pvParameters)
 {
+    TickType_t lastWakeTime ;
+    TickType_t transmitPeriod = pdMS_TO_TICKS((uint32_t) pvParameters);
+    BmsContactorRequest_t bmsContactorRequestState = CONTACTOR_OFF;
+    PcuFault_t pcuFault = NO_PCU_FAULT;
+    CriticalPcuFault_t criticalPcuFault = NO_CRITICAL_PCU_FAULT;
+    canMessage_t bmsContactorRequest =
+    {
+     .id  = BMS_CONTACTOR_REQUEST,
+     .dlc = BMS_CONTACTOR_REQUEST_DLC,
+     .ide = (uint8_t)CAN_Id_Extended,
+    };
+
+    memset(bmsContactorRequest.data, 0, sizeof(bmsContactorRequest.data));
+
+    lastWakeTime = xTaskGetTickCount();
     while(1)
     {
-        taskYIELD();
+        setBmsContactorRequest(&bmsContactorRequest,(uint8_t) bmsContactorRequestState);
+        setPcuCriticalFault(&bmsContactorRequest,(uint8_t) criticalPcuFault);
+        setPcuFault(&bmsContactorRequest,(uint8_t) pcuFault);
+        newCanTransmit(canREG1, canMESSAGE_BOX1, &bmsContactorRequest);
+
+        vTaskDelayUntil(&lastWakeTime, transmitPeriod);
     }
 }
 
 void vBmsHeartBeat (void *pvParameters)
 {
     TickType_t lastWakeTime ;
-    canMessage_t canHearBeat =
+    TickType_t transmitPeriod = pdMS_TO_TICKS((uint32_t) pvParameters);
+    canMessage_t bmsHearBeat =
     {
-     .id = 0x01,
-     .dlc = 8,
-     .ide = (uint8_t)CAN_Id_Standard,
+     .id  = BMS_HEARTBEAT,
+     .dlc = BMS_HEARTBEAT_DLC,
+     .ide = (uint8_t)CAN_Id_Extended,
     };
+
+    memset(bmsHearBeat.data, 0, sizeof(bmsHearBeat.data));
+
     lastWakeTime = xTaskGetTickCount();
     while(1)
     {
-        newCanTransmit(canREG1, canMESSAGE_BOX1, &canHearBeat);
-      //  newCanTransmit(canREG2, canMESSAGE_BOX2, 0xff5f5, canHearBeat.data, canHearBeat.dlc, CAN_Id_Extended);
-       // newCanTransmit(canREG1, canMESSAGE_BOX2, 0xf, canHearBeat.data, canHearBeat.dlc, CAN_Id_Standard);
+        newCanTransmit(canREG1, canMESSAGE_BOX1, &bmsHearBeat);
 
-        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(100U));
+        vTaskDelayUntil(&lastWakeTime, transmitPeriod);
     }
 }
 
